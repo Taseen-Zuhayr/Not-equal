9656dfab41ab783fb0dd9cfcb18803293ddf5299
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-14 22:44:48 +0600

    Equals

---
+++ main.py
@@ -0,0 +1,17 @@
+#Activity_1
+a = int(input("Input the first number : "))
+b = int(input("Input the second number : "))
+
+if a!=b:
+    print("Not equal.")
+else:
+    print("Equal.")
+
+#Activity_2
+x = input("Input your name : ")
+y = input("input your friend's name : ")
+
+if x!=y:
+    print("The names are not simiar.")
+else:
+    print("The names are similar")
